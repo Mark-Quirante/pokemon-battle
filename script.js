20620b9608598cd5962b172aa05938ddac78bb4e
Mark Quirante <quir0054@algonquinlive.com>
2024-07-13 22:42:32 -0400

    Added a generate button that makes 2 unique pokemons

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
 // Function to fetch data from the API
-async function fetchData() {
+async function fetchData(id, pokemonName) {
 	try {
-		const response = await fetch(apiUrl + generatePokemon()); // Make the API request
+		const response = await fetch(apiUrl + pokemonName); // Make the API request
 
 		if (!response.ok) {
 			// Check if the request was successful
@@ -16,7 +16,7 @@ async function fetchData() {
 
 		const imgURL = data.sprites.front_default;
 		// Example: Update the DOM with the fetched data
-		const dataContainer = document.getElementById("pokemon");
+		const dataContainer = document.getElementById(id);
 
 		dataContainer.src = imgURL;
 		dataContainer.alt = data.name;
@@ -25,11 +25,125 @@ async function fetchData() {
 	}
 }
 
-function generatePokemon() {
-	const pokemonArray = ["charizard", "pikachu", "blastoise"];
+function generatePokemonName() {
+	const pokemonArray = [
+		"bulbasaur",
+		"ivysaur",
+		"venusaur",
+		"charmander",
+		"charmeleon",
+		"charizard",
+		"squirtle",
+		"wartortle",
+		"blastoise",
+		"caterpie",
+		"metapod",
+		"butterfree",
+		"weedle",
+		"kakuna",
+		"beedrill",
+		"pidgey",
+		"pidgeotto",
+		"pidgeot",
+		"rattata",
+		"raticate",
+		"spearow",
+		"fearow",
+		"ekans",
+		"arbok",
+		"pikachu",
+		"raichu",
+		"sandshrew",
+		"sandslash",
+		"nidoran♀",
+		"nidorina",
+		"nidoqueen",
+		"nidoran♂",
+		"nidorino",
+		"nidoking",
+		"clefairy",
+		"clefable",
+		"vulpix",
+		"ninetales",
+		"jigglypuff",
+		"wigglytuff",
+		"zubat",
+		"golbat",
+		"oddish",
+		"gloom",
+		"vileplume",
+		"paras",
+		"parasect",
+		"venonat",
+		"venomoth",
+		"diglett",
+		"dugtrio",
+		"meowth",
+		"persian",
+		"psyduck",
+		"golduck",
+		"mankey",
+		"primeape",
+		"growlithe",
+		"arcanine",
+		"poliwag",
+		"poliwhirl",
+		"poliwrath",
+		"abra",
+		"kadabra",
+		"alakazam",
+		"machop",
+		"machoke",
+		"machamp",
+		"bellsprout",
+		"weepinbell",
+		"victreebel",
+		"tentacool",
+		"tentacruel",
+		"geodude",
+		"graveler",
+		"golem",
+		"ponyta",
+		"rapidash",
+		"slowpoke",
+		"slowbro",
+		"magnemite",
+		"magneton",
+		"farfetch'd",
+		"doduo",
+		"dodrio",
+		"seel",
+		"dewgong",
+		"grimer",
+		"muk",
+		"shellder",
+		"cloyster",
+		"gastly",
+		"haunter",
+		"gengar",
+		"onix",
+		"drowzee",
+		"hypno",
+		"krabby",
+		"kingler",
+		"voltorb",
+		"electrode",
+	];
 	const pokemonIndex = Math.floor(Math.random() * pokemonArray.length);
 	return pokemonArray[pokemonIndex];
 }
 
-// Call the fetchData function to initiate the request
-fetchData();
+function generatePokemon() {
+	const name1 = generatePokemonName();
+	const name2 = generatePokemonName();
+
+	if (name1 === name2) {
+		name2 = generatePokemonName();
+	}
+	fetchData("pokemon1", name1);
+	fetchData("pokemon2", name2);
+}
+
+const pokemonButton = document
+	.getElementById("btn")
+	.addEventListener("click", generatePokemon);
